627c5295c79cfa3d95abda1d759498b5f00d8ce6
dangge2 <dangge2@gmail.com>
2017-09-03 21:22:31 +0800

    update todo

--- app/views.py
+++ app/views.py
@@ -1,8 +1,34 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 from app.models import Todo
+
+
 @app.route('/')
 def index():
     todos = Todo.objects.all()
     return render_template("index.html", todos=todos)
 
+@app.route('/add', methods=['POST', ])
+def add():
+    content = request.form.get("content")
+    todo = Todo(content=content)
+    todo.save()
+    todos = Todo.objects.all()
+    return render_template("index.html", todos=todos)
+
+@app.route('/done/<string:todo_id>')
+def done(todo_id):
+    todo = Todo.objects.get_or_404(id=todo_id)
+    todo.status = 1
+    todo.save()
+    todos = Todo.objects.all()
+    return render_template("index.html", todos=todos)
+
+
+@app.route('/undone/<string:todo_id>')
+def undone(todo_id):
+    todo = Todo.objects.get_or_404(id=todo_id)
+    todo.status = 0
+    todo.save()
+    todos = Todo.objects.all()
+    return render_template("index.html", todos=todos)
